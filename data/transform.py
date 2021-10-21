Pipeline with logit logging and data masking fixes

data/transform.py
@@ -7,7 +7,7 @@ class MaskTransform(T.BaseTransform):
 
     def __init__(self, mask):
         super().__init__()
-        self.mask = mask
+        self.mask = torch.tensor(mask).bool()
     
     @torch.no_grad()
     def __call__(self, data):
@@ -15,11 +15,21 @@ class MaskTransform(T.BaseTransform):
         return data
 
 class MaskLabelsTransform(T.BaseTransform):
-    """ Sets a mask that only selects certain labels. """
+    """ Sets a mask that only selects certain labels. 
     
-    def __init__(self, select_labels):
+    Parameters:
+    -----------
+    select_labels : iterable
+        All labels that the mask in the dataset will select.
+    remap_labels : bool
+        If True, these selected labels will be remapped to indices (0, 1, ..., k).
+        This way no problems occur when training.
+    """
+    
+    def __init__(self, select_labels, remap_labels=True):
         super().__init__()
-        self.select_labels = select_labels
+        self.select_labels = tuple(select_labels)
+        self.remap_labels = remap_labels
         
     @torch.no_grad()
     def __call__(self, data):
@@ -28,6 +38,20 @@ class MaskLabelsTransform(T.BaseTransform):
             mask[data.y == label] = True
         mask &= data.mask
         data.mask = mask
+
+        if self.remap_labels: # Remap the labels
+            for num, label in enumerate(self.select_labels):
+                # Swap label `label` and `num`
+                idx_num, idx_label = data.y == num, data.y == label
+                data.y[idx_num] = label
+                data.y[idx_label] = num
+                # Change `data.label_to_idx` accordingly
+                idx_to_label = {v : k for k, v in data.label_to_idx.items()}
+                label_to_idx = data.label_to_idx.copy()
+                label_to_idx[idx_to_label[num]] = label
+                label_to_idx[idx_to_label[label]] = num
+                data.label_to_idx = label_to_idx
+
         return data
 
 class IdentityTransform(T.BaseTransform):

data/util.py
@@ -8,28 +8,6 @@ from data.gust_dataset import GustDataset
 
 from seed import data_split_seeds
 
-# class SplitDataset(Dataset):
-#     """ Dataset wrapper after splitting. """
-
-#     def __init__(self, base_dataset, mask, copy_data=True):
-#         self.copy_data = copy_data
-#         self.base_dataset = base_dataset
-#         self.mask = torch.tensor(mask)
-    
-#     def __len__(self):
-#         return len(self.base_dataset)
-    
-#     def __getitem__(self, idx):
-#         data = self.base_dataset[idx]
-#         if self.copy_data:
-#             data = data.clone()
-#         data.mask = self.mask
-#         return data
-
-
-
-
-
 def stratified_split_with_fixed_test_set_portion(ys, num_splits, portion_train=0.05, portion_val=0.15, portion_test_fixed=0.2, portion_test_not_fixed=0.6):
     """ Splits the dataset using a stratified strategy into training, validation and testing data.
     A certain portion of the testing data will be fixed and shared among all splits.
@@ -57,6 +35,7 @@ def stratified_split_with_fixed_test_set_portion(ys, num_splits, portion_train=0
         Mask for test data that is fixed among all splits.
     """
     seeds = data_split_seeds(num_splits + 1) # The first seed is used to fix the shared testing data
+    assert np.allclose(portion_train + portion_val + portion_test_not_fixed + portion_test_fixed, 1.0), f'Dataset splits dont add to 1.0'
     norm = portion_train + portion_val + portion_test_not_fixed
     mask_non_fixed, mask_fixed = stratified_split(ys, seeds[0:1], [norm, 1 - norm])[:, 0, :]
 
@@ -285,7 +264,6 @@ def _load_gust_data_from_configuration(config):
                                                            portion_test_fixed=config['test_portion_fixed'], 
                                                            portion_test_not_fixed=config['test_portion'],
                                                            )
-
     return [
         [GustDataset(config['dataset'], transform=MaskTransform(mask[type_idx, split_idx])) for type_idx in (0, 1, 1, 2)] for split_idx in range(mask.shape[1])
         ], GustDataset(config['dataset'], transform=MaskTransform(mask_test_fixed))
@@ -309,6 +287,7 @@ def load_data_from_configuration(config):
         data_list, dataset_fixed = _load_gust_data_from_configuration(config)
     else:
         raise RuntimeError(f'Unsupported dataset type {config["dataset"]}')
+
     if config.get('train_labels', 'all') != 'all':
         # Select only certain labels from training data
         print(f'Reducing train labels to {config["train_labels"]}')

evaluation/pipeline.py
@@ -20,6 +20,7 @@ class EvaluateEmpircalLowerLipschitzBounds:
         self.seed = None
         self.gpus = gpus
 
+    @torch.no_grad()
     def __call__(self, *args, **kwargs):
         assert len(kwargs['data_loader_val']) == 1, f'Empirical local lipschitz evaluation is currently only supported for semi-supervised tasks.'
         for dataset in kwargs['data_loader_val']:
@@ -29,18 +30,17 @@ class EvaluateEmpircalLowerLipschitzBounds:
             dataset = dataset.to('cuda') # A bit hacky, but short...7
             kwargs['model'] = kwargs['model'].to('cuda')
 
-
         perturbations = evaluation.lipschitz.local_perturbations(kwargs['model'], dataset,
             perturbations=np.linspace(self.min_perturbation, self.max_perturbation, self.num_perturbations),
             num_perturbations_per_sample=self.num_perturbations_per_sample, seed=self.seed)
         pipeline_log(f'Created {self.num_perturbations_per_sample} perturbations in linspace({self.min_perturbation:.2f}, {self.max_perturbation:.2f}, {self.num_perturbations}) for validation samples.')
         smean, smedian, smax, smin = evaluation.lipschitz.local_lipschitz_bounds(perturbations)
-        kwargs['logger'].log_metrics = {
+        kwargs['logger'].log_metrics({
             'slope_mean_perturbation' : smean,
             'slope_median_perturbation' : smedian,
             'slope_max_perturbation' : smax,
             'slope_min_perturbation' : smin,
-        }
+        })
         # Plot the perturbations and log it
         fig, _ , _, _ = plot.perturbations.local_perturbations_plot(perturbations)
         if isinstance(kwargs['logger'], TensorBoardLogger):
@@ -62,16 +62,14 @@ class FitLogitDensity:
             raise RuntimeError(f'Unsupported logit space density {density_type}')
         self.gpus = gpus
 
+    @torch.no_grad()
     def __call__(self, *args, **kwargs):
         assert len(kwargs['data_loader_train']) == 1 and len(kwargs['data_loader_val_all_classes']) == 1, f'Logit Space Density is currently only supported for semi-supervised tasks.'
         for data_train in kwargs['data_loader_train']:
             break # We just want the single dataset
-        for data_val in kwargs['data_loader_val_all_classes']:
-            break # We just want the single dataset
     
         if self.gpus > 0:
             data_train = data_train.to('cuda')
-            data_val = data_val.to('cuda')
             kwargs['model'] = kwargs['model'].to('cuda')
 
         logits = kwargs['model'](data_train)[-1][data_train.mask]
@@ -81,6 +79,70 @@ class FitLogitDensity:
         kwargs['logit_density'] = self.density
         pipeline_log(f'Fitted density of type {self.density_type} to training data logits.')
         return args, kwargs
+
+class EvaluateLogitDensity:
+    """ 
+    Pipeline member that evaluates the logit density at each sample in the validation set.
+    It also logs histograms and statistics.
+    """
+
+    name = 'EvaluateLogitDensity'
+
+    def __init__(self, gpus=0):
+        self.gpus = gpus
+    
+    @torch.no_grad()
+    def __call__(self, *args, **kwargs):
+        for data_val in kwargs['data_loader_val_all_classes']:
+            break # We just want the single dataset
+
+        if self.gpus > 0:
+            data_val = data_val.to('cuda')
+            kwargs['model'] = kwargs['model'].to('cuda')
+
+        logits = kwargs['model'](data_val)[-1][data_val.mask].cpu()
+        density = kwargs['logit_density'].cpu()(logits)
+        
+        # Log histograms and metrics label-wise
+        y = data_val.y[data_val.mask]
+        for label in torch.unique(y):
+            density_label = density[y == label]
+            if isinstance(kwargs['logger'], TensorBoardLogger):
+                kwargs['logger'].experiment.add_histogram(f'logit_density', density_label, global_step=label)
+            kwargs['logger'].log_metrics({
+                f'mean_logit_density' : density_label.mean(),
+                f'std_logit_density' : density_label.std(),
+                f'min_logit_density' : density_label.min(),
+                f'max_logit_density' : density_label.max(),
+                f'median_logit_density' : density_label.median(),
+            }, step=label)
+        pipeline_log(f'Evaluated logit density for entire validation dataset (labels : {torch.unique(y).cpu().tolist()}).')
+        return args, kwargs
+
+class LogLogits:
+    """ Pipeline member that logs the logits of the validation data. """
+
+    name = 'LogLogits'
+
+    def __init__(self, gpus=0):
+        self.gpus = gpus
+    
+    @torch.no_grad()
+    def __call__(self, *args, **kwargs):
+        for data_val in kwargs['data_loader_val_all_classes']:
+            break # We just want the single dataset
+
+        if self.gpus > 0:
+            data_val = data_val.to('cuda')
+            kwargs['model'] = kwargs['model'].to('cuda')
+
+        logits = kwargs['model'](data_val)[-1][data_val.mask].cpu()
+        if isinstance(kwargs['logger'], TensorBoardLogger):
+            kwargs['logger'].experiment.add_embedding(logits, tag='val_logits')
+        pipeline_log(f'Logged logits (size {logits.size()}) of entire validation dataset.')
+    
+        return args, kwargs
+
 
 # class SelectClassLabels:
 #     """ Pipeline element that selects only a set of class labels for a given dataset. """
@@ -127,6 +189,15 @@ class Pipeline:
                     density_type='gmm',
                     gpus = gpus,
                 ))
+            elif name.lower() == EvaluateLogitDensity.name.lower():
+                self.members.append(EvaluateLogitDensity(
+                    gpus=gpus,
+                ))
+            elif name.lower() == LogLogits.name.lower():
+                self.members.append(LogLogits(
+                    gpus=gpus,
+                ))
+
             # elif name.lower() == 'selectclasslabelstrain':
             #     self.members.append(SelectClassLabels(
             #         select_labels=config['select_class_labels_train'],